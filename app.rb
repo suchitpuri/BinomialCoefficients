c896db9329f49695b5ae3c0fb7b99d3e85923dfb
Suchit Puri <suchitp@thoughtworks.com>
2012-10-07 20:26:19 +0530

    finally super , working solution for input size 10^500 yay

--- app.rb
+++ app.rb
@@ -1,28 +1,12 @@
-#class Permutation
-  #@@store = {}
-
-  #def self.fact_recursion num
-    #return 1 if(num == 0)
-    #return @@store[num] unless  @@store[num].nil?
-    #fact =  (num * fact_recursion(num-1))
-    #@@store[num] = fact
-    #fact
-  #end
-
-  #def self.getCache
-    #@@store
-  #end
-
-#end
-
-def Math.nCr(n,r)
-  a, b = r, n-r
-  a, b = b, a if a < b  # a is the larger
-  numer = (a+1..n).inject(1) { |t,v| t*v }  # n!/r!
-  denom = (2..b).inject(1) { |t,v| t*v }    # (n-r)!
-  numer/denom
+def divisibleCoefficients(n,p)
+  m = n 
+  nondiv = 1
+  while m > 0
+    nondiv = nondiv * ((m % p) + 1)
+    m = m / p
+  end
+  return (n + 1 - nondiv)
 end
-
 
 number_of_test_cases = 0;
 input = []
@@ -39,13 +23,7 @@ number_of_test_cases.times do |i|
  result = 0;
  n = input[i].split(" ").first.to_i;
  prime = input[i].split(" ").last.to_i;
- #binding.pry
- (1..n).each do |i|
-  if(((Math.nCr(n , i)) % prime) == 0)
-    result = result + 1
-  end
- end
- p result
+ p divisibleCoefficients(n,prime)
 end
 
 
